d07a3af381feb076af0188b397ce8fb45beb3530
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-08-07 12:48:06 +0600

    design updated

---
+++ lib/view/screen/onboard_screen/onboard_page1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnboardPage1 extends StatelessWidget {
+  const OnboardPage1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
